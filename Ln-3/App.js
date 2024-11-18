1baf48a7f6ca95653ef956f1d69e9011e58a75ae
Shankaranarayanansk <sankaranarayananit002@gmail.com>
2024-11-18 21:10:01 +0530

    Episode 3

--- Ln-3/App.js
+++ Ln-3/App.js
@@ -1,25 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Elem = (<p>memo </p>)
-const ReactElement = <h1>hello la <span>{Elem}</span> </h1>;
+const Elem = <p>memo</p>;
+
+const ReactElement = (
+  <h1>
+    hello la <span>{Elem}</span>
+  </h1>
+);
 
 const Heading = () => {
   return <p>Paratag</p>;
 };
+
 const Title = () => (
-  
   <h1>
     hello ala
-    {7+8}
-    {ReactElement}// rendering react element
+    {7 + 8}
+    {ReactElement}
     <Heading />
   </h1>
-)
-const Tit = <span>
-    <Title/>
-</span>
+);
 
+const App = () => (
+  <div>
+    <Title />
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Tit />);
+root.render(<App />);
